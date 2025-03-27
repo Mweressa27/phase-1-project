d26c251d6da6abea9c97b4f4e41ad280d1994f25
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-27 12:20:30 +0300

    added images

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const landingImage = document.getElementById('landingImage');  
-
-    if (landingImage) {
+if (landingImage) {
         const imageSources = [
             './images/landing_page_img.jpg',
             './images/landing_page_img1.jpg',
@@ -21,6 +20,30 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error('Landing image element not found!');
     }
 
-       
-  
+function displayBooks (){
+    fetch('http://localhost:3000/books?_limit=4')
+    .then(res => res.json())
+    .then(books => {
+        const specialOffersSection = document.querySelector('.special-offers');
+        specialOffersSection.innerHTML = '';
+
+        books.forEach(book => {
+const bookDiv = document.createElement('div');
+bookDiv.classList.add('special-offers-items');
+bookDiv.innerHTML = `
+                <img src="${book.cover_page}" alt="${book.title}" width="150px" height="250px">
+                <p>${book.title}</p>
+                <h2><span>${book.price}</span> KSH.${parseInt(book.price.replace('Ksh. ', '').replace(',', '')) - 200}</h2>
+                <button>Add to cart</button>
+                <img src="./images/logos/wishlist.png" alt="Wishlist" width="30px" height="30px">
+`;
+specialOffersSection.appendChild(bookDiv);
+        }) 
+
+    })
+    .catch(error => {
+        console.error('Error fetching books data:', error);
+    });
+}
+displayBooks ()
 });
